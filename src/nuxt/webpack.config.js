39cdb89ac3c61872ed73d31d17bffdc059c85cb4
Dawid Charzewski <dawidchar1@gmail.com>
2023-06-24 13:18:50 +0100

    Add prod config 🌐

--- src/nuxt/webpack.config.js
+++ src/nuxt/webpack.config.js
@@ -1,32 +1,41 @@
 const path = require('path')
+const webpack = require('webpack')
 
-const config = {
-    name: 'static_js_files',
-    entry: {
-        sw_firebase_auth: './static/sw_firebase_auth.js'
-    },
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, './static/dist')
-    },
-    resolve: {
-        alias: {
-            '~': path.resolve(__dirname, './')
+const config = (env, argv) => {
+    const isProduction = argv.mode === 'production'
+
+    return {
+        name: 'static_js_files',
+        entry: {
+            sw_firebase_auth: './static/sw_firebase_auth.js'
         },
-        extensions: ['.js']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/
-            }
-        ]
-    },
-    mode: 'none',
-    optimization: {
-        minimize: true
+        output: {
+            filename: '[name].js',
+            path: path.resolve(__dirname, './static/dist')
+        },
+        resolve: {
+            alias: {
+                '~': path.resolve(__dirname, './')
+            },
+            extensions: ['.js']
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    loader: 'babel-loader',
+                    exclude: /node_modules/
+                }
+            ]
+        },
+        plugins: [
+            new webpack.DefinePlugin({
+                'process.env.NODE_ENV': JSON.stringify(isProduction ? 'production' : 'development')
+            })
+        ],
+        optimization: {
+            minimize: true
+        }
     }
 }
 
